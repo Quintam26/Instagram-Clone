1d36267aed20fc06da53bfab238b538ed3b75795
Mario Quintana <pyrros2000@gmail.com>
2020-03-01 19:22:33 -0800

    username appears on tab when logged in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Login from './components/Login';
 import Header from './components/Header';
+import CreatePost from './components/CreatePost';
 
 function App() {
   const [user, setUser] = React.useState('');
+
+  React.useEffect(() => {
+    document.title = user ? `${user}'s feed` : 'Please login';
+  }, [user]);
 
   if (!user) {
     return <Login setUser={setUser} />;
   }
   return (
     <>
-      <Header user={user} setUser={setUser}/>
+      <Header user={user} setUser={setUser} />
+      <CreatePost />
     </>
   );
 }
