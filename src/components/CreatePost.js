7ff166fdb44cf486aaaccfb09b68e118434504c7
Mario Quintana <pyrros2000@gmail.com>
2020-03-08 12:28:11 -0700

    Added new state for content and image to render posts through component

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-function CreatePost() {
+function CreatePost({ user, setPosts, posts }) {
   const [content, setContent] = React.useState('');
   const [image, setImage] = React.useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const post = { content, image, user };
+    const newPosts = [post, ...posts];
+    setPosts(newPosts);
+  }
 
   return (
     <div>
       <h1>Create New Post</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           placeholder='Add Post Content'
@@ -19,12 +26,6 @@ function CreatePost() {
         />
         <button type='submit'>Submit Post</button>
       </form>
-      <p>{content}</p>
-      {image && <img
-        style={{ height: 200, width: 300, objectFit: 'cover' }}
-        src={URL.createObjectURL(image)}
-        alt='N/A'
-      />}
     </div>
   );
 }
