437e49e4c8337457efbcd2fc20cf95be4fc06e47
Mario Quintana <pyrros2000@gmail.com>
2020-03-22 21:42:30 -0700

    consume context with usecontext , avoid props drilling and repetition, cleaned up Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { UserContext } from '../App';
 
 function Post({ image, content, user }) {
+  const currentUser = React.useContext(UserContext);
+  const isCurrentUser = currentUser === user;
+
   return (
-    <UserContext.Consumer>
-      {currentUser => (
-        <>
-          {image && (
-            <img
-              style={{ height: 200, width: 300, objectFit: 'cover' }}
-              src={URL.createObjectURL(image)}
-              alt='Post Cover'
-            />
-          )}
-          <p>{content}</p>
-          <div style={{ color: currentUser === user && 'green' }}>{user}</div>
-        </>
+    <>
+      {image && (
+        <img
+          style={{ height: 200, width: 300, objectFit: 'cover' }}
+          src={URL.createObjectURL(image)}
+          alt='Post Cover'
+        />
       )}
-    </UserContext.Consumer>
+      <p>{content}</p>
+      <div style={{ color: isCurrentUser && 'red' }}>{user}</div>
+    </>
   );
 }
 
